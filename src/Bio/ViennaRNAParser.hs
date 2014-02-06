aee564edd6e343b719b9444cf34c92fcacbe52a5
Florian Eggenhofer <egg@tbi.univie.ac.at>
2014-02-06 14:45:33 +0100

    Added file that reexports libary functions

---
+++ src/Bio/ViennaRNAParser.hs
@@ -0,0 +1,20 @@
+-- | Parsers for Programs from the Vienna RNA Package
+-- this libary is focused on Version > 2.0 
+-- For more information on the Vienna RNAPackage consult: <http://www.tbi.univie.ac.at/~ivo/RNA/>
+-- or the homepage of the current maintainer: <http://www.tbi.univie.ac.at/~ronny/RNA/index.html>
+
+module Bio.ViennaRNAParser (                            
+                       module Bio.RNAplexData,
+                       module Bio.RNAplexParser,
+                       module Bio.RNAzData,
+                       module Bio.RNAzParser
+                      ) where
+
+import Bio.RNAplexData
+import Bio.RNAplexParser
+import Bio.RNAzData
+import Bio.RNAzParser
+import Bio.RNAfoldData
+import Bio.RNAfoldParser
+
+
