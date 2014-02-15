c46d872fc24d4066301d0ae07002d89969f2dead
Florian Eggenhofer <egg@tbi.univie.ac.at>
2014-02-15 15:13:55 +0100

    Added RNAalifold prototype parsing function

--- src/Bio/RNAalifoldData.hs
+++ src/Bio/RNAalifoldData.hs
@@ -2,10 +2,10 @@
 --   For more information on RNAalifold consult: <>
 
 module Bio.RNAalifoldData where
+
+-- | Data structure corrsponds with alifoldoutput redirected to file. Output written directly to commandline also contains number of sequences and length of consensus sequence
 data RNAalifoldOutput = RNAalifoldOutput
   {
-    sequenceNumber :: Int,
-    alignmentLength :: Int,
     alignmentConsensusSequence :: String,
     alignmentConsensusDotBracket :: String,
     alignmentConsensusMinimumFreeEnergy :: Double,
---
+++ src/Bio/RNAalifoldParser.hs
@@ -0,0 +1,47 @@
+-- | Parse RNAalifold output
+--   For more information on RNAalifold consult: <>
+
+module Bio.RNAalifoldParser (
+                       parseRNAalifold,
+                       readRNAalifold,                                   
+                       module Bio.RNAalifoldData
+                      ) where
+
+import Bio.RNAalifoldData
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Token
+import Text.ParserCombinators.Parsec.Language (emptyDef)    
+import Control.Monad
+
+readDouble :: String -> Double
+readDouble = read              
+
+readInt :: String -> Int
+readInt = read
+
+-- | Parse the consenus of RNAz results         
+genParserRNAalifold :: GenParser Char st RNAalifoldOutput
+genParserRNAalifold = do
+  sequence <- many1 (noneOf "\n")                
+  newline
+  secondaryStructure <- many1 (oneOf "&().,")
+  string (" (")
+  foldingEnergy <- many1 (noneOf " ")
+  string " ="
+  many1 space
+  initialFoldingEnergy <- many1 (noneOf " ")
+  string (" +")
+  many1 space
+  covarianceContributionEnergy <- many1 (noneOf ")")
+  string (")")
+  many1 space
+  newline
+  return $ RNAalifoldOutput sequence secondaryStructure (readDouble foldingEnergy) (readDouble initialFoldingEnergy) (readDouble covarianceContributionEnergy)   
+
+-- | parse RNAalifold output from input string
+parseRNAalifold input = parse genParserRNAalifold "genParseRNAalifold" input
+
+-- | parse RNAalifold output from input filePath                      
+readRNAalifold :: String -> IO (Either ParseError RNAalifoldOutput)                  
+readRNAalifold filePath = parseFromFile genParserRNAalifold filePath
+
