487c1333618c87900f7c832c994d1882e604dd5a
Florian Eggenhofer <egg@tbi.univie.ac.at>
2015-06-22 11:22:50 +0200

    Added changed type names to tests

--- test-suite/ViennaRNAParserSpec.hs
+++ test-suite/ViennaRNAParserSpec.hs
@@ -63,8 +63,8 @@ rnaalifoldErrorSourcePosition = newPos "genParseRNAalifold" 1 14
 rnaalifoldexample :: String
 rnaalifoldexample = "__UGGUGGC__CAUAGC_GGA_G_G__G_G_AAA_C_ACCCGU_U__C_CCAUUCCGAACACGGAAGUUAA_GC_CCUUCAGC___G___C___C____G___A___UG___G___UACUGCGGGGG___AA___C________GGCGUG_GGA_G_A_GU_A__G_G_UCGCCGCC_A_G___\n.((((((((......((.(((.(.(..............((((.................))))...........))))).))...................................(((((((...................)))))).)...................)))))).).)... (-28.28 = -23.14 +  -5.14)\n"
 
-rnaalifoldresult :: RNAalifoldOutput
-rnaalifoldresult = RNAalifoldOutput "__UGGUGGC__CAUAGC_GGA_G_G__G_G_AAA_C_ACCCGU_U__C_CCAUUCCGAACACGGAAGUUAA_GC_CCUUCAGC___G___C___C____G___A___UG___G___UACUGCGGGGG___AA___C________GGCGUG_GGA_G_A_GU_A__G_G_UCGCCGCC_A_G___" ".((((((((......((.(((.(.(..............((((.................))))...........))))).))...................................(((((((...................)))))).)...................)))))).).)..." (-28.28) (-23.14) (-5.14) 
+rnaalifoldresult :: RNAalifold
+rnaalifoldresult = RNAalifold "__UGGUGGC__CAUAGC_GGA_G_G__G_G_AAA_C_ACCCGU_U__C_CCAUUCCGAACACGGAAGUUAA_GC_CCUUCAGC___G___C___C____G___A___UG___G___UACUGCGGGGG___AA___C________GGCGUG_GGA_G_A_GU_A__G_G_UCGCCGCC_A_G___" ".((((((((......((.(((.(.(..............((((.................))))...........))))).))...................................(((((((...................)))))).)...................)))))).).)..." (-28.28) (-23.14) (-5.14) 
 
 rnadistanceexample :: String
 rnadistanceexample = "f: 96"
@@ -144,8 +144,8 @@ rnaupErrorSourcePosition = newPos "parseRNAupOutput" 1 1
 rnaZexample :: String
 rnaZexample = "############################  RNAz 2.1  ##############################\n\n\n Sequences: 4\n Columns: 73\n Reading direction: forward\n Mean pairwise identity:  80.82\n Shannon entropy: 0.31118\n G+C content: 0.54795\n Mean single sequence MFE: -27.20\n Consensus MFE: -26.50\n Energy contribution: -23.63\n Covariance contribution:  -2.87\n Combinations/Pair:   1.43\n Mean z-score:  -1.82\n Structure conservation index:   0.97\n Background model: dinucleotide\n Decision model: sequence based alignment quality\n SVM decision value:   2.15\n SVM RNA-class probability: 0.984068\n Prediction: RNA\n\n######################################################################\n\n>sacCer1.chr4 1352453 73 - 1531914\nGCCUUGUUGGCGCAAUCGGUAGCGCGUAUGACUCUUAAUCAUAAGGUUAGGGGUUCGAGCCCCCUACAGGGCU\n(((((((.(((((........))))...((((.((((....))))))))(((((....)))))).))))))). ( -29.20, z-score =  -2.35, R)\n>sacBay.contig_465 14962 73 - 57401\nGCCUUGUUGGCGCAAUCGGUAGCGCGUAUGACUCUUAAUCAUAAGGUUAGGGGUUCGAGCCCCCUACAGGGCU\n(((((((.(((((........))))...((((.((((....))))))))(((((....)))))).))))))). ( -29.20, z-score =  -2.35, R)\n>consensus\nGCCUUGUUGGCGCAAUCGGUAGCGCGUAUGACUCUUAAUCAUAAGGUUAGGGGUUCGAGCCCCCUACAGGGCU\n(((((((..((((........)))).(((((.......))))).....(((((.......)))))))))))). (-26.50 = -23.63 +  -2.87)\n"
 
-rnaZresult :: RNAzOutput
-rnaZresult = RNAzOutput "RNAz 2.1" 4 73 "forward" 80.82 0.31118 0.54795 (-27.20) (-26.50) (-23.63) (-2.87) 1.43 (-1.82) 0.97 "dinucleotide" "sequence based alignment quality" 2.15 0.984068 "RNA" rnaZEntries rnaZconsenus
+rnaZresult :: RNAz
+rnaZresult = RNAz "RNAz 2.1" 4 73 "forward" 80.82 0.31118 0.54795 (-27.20) (-26.50) (-23.63) (-2.87) 1.43 (-1.82) 0.97 "dinucleotide" "sequence based alignment quality" 2.15 0.984068 "RNA" rnaZEntries rnaZconsenus
 
 rnaZEntries :: [RNAzResult]
 rnaZEntries = [RNAzResult "sacCer1.chr4 1352453 73 - 1531914" "GCCUUGUUGGCGCAAUCGGUAGCGCGUAUGACUCUUAAUCAUAAGGUUAGGGGUUCGAGCCCCCUACAGGGCU" "(((((((.(((((........))))...((((.((((....))))))))(((((....)))))).)))))))." (-29.20) (-2.35) 'R',RNAzResult "sacBay.contig_465 14962 73 - 57401" "GCCUUGUUGGCGCAAUCGGUAGCGCGUAUGACUCUUAAUCAUAAGGUUAGGGGUUCGAGCCCCCUACAGGGCU" "(((((((.(((((........))))...((((.((((....))))))))(((((....)))))).)))))))." (-29.20) (-2.35) 'R']
